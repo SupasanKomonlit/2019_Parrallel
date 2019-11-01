007fa0f9249f43ac24eb11103c8e5f9a411e2a9a
Supasan Komonlit <supasan_224@hotmail.com>
2019-11-01 12:41:41 +0700

    Waiting for testing and debug code

--- cuda/stream_matrix_multi.c
+++ cuda/stream_matrix_multi.c
@@ -20,6 +20,26 @@
 
 typedef int data_type;
 
+__global__
+void kernalMulMatrixTranspose( data_type* matrix_a,
+        data_type* matrix_b_t,
+        data_type* matrix_c,
+        unsigned int same_size,
+        unsigned int offset_row_index,
+        unsigned int offset_column_index )
+{
+    data_type result = 0;
+    // I will design row_index , column_index that is tile index
+    //  Tile will have dimension 32 * 32 only
+    unsigned int a_index = offset_row_index + threadIdx.y;
+    unsigned int b_index = offset_column_index + threadIdx.x;
+    for( unsigned int run = 0 ; run < same_size ; run++ )
+    {
+        result *= matrix_a[ a_index + run ] * matrix_b_t[ b_index + run ];
+    } 
+    matrix_c[ a_index * same_size + b_index ] = result;
+} // kernalMulMatrixTranspose
+
 void mulMatrixTranspose( data_type* matrix_a, 
         data_type* matrix_b_t, 
         data_type* matrix_c,
@@ -43,7 +63,7 @@ void mulMatrixTranspose( data_type* matrix_a,
             matrix_c[ row * column_size + column] = result_value;
         }
     }
-}
+} // mulMatrixTranspose
 
 void transposeMatrix( data_type* origin_matrix,
         data_type* transpose_matrix,
@@ -61,10 +81,11 @@ void printMatrix( data_type* ptr_matrix , unsigned int row_size , unsigned int c
 
 int main()
 {
-    unsigned int column_a = 64;
-    unsigned int row_a = 64;
-    unsigned int column_b = 64;
-    unsigned int row_b = 64;
+    // Assum size of matrix m * n is row * column
+    unsigned int column_a = 4096;
+    unsigned int row_a = 4096;
+    unsigned int column_b = 4096;
+    unsigned int row_b = 4096;
     unsigned int column_c = column_a;
     unsigned int row_c = row_b;
     
@@ -78,21 +99,102 @@ int main()
     ptr_matrix_c = ( data_type* ) malloc( column_c * row_c * sizeof( data_type ) );
     ptr_matrix_b_t = ( data_type* ) malloc( column_b * row_b * sizeof( data_type ) );    
 
+    // Assume Dimension more than 1024 for all dimension
+    dim3 block_dimension( 32 , 32 , 0 );
+    dim3 grid_dimension( row_a / 32 . column_b / 32 );
+
     randomMatrix( ptr_matrix_a, row_a , column_a );
     randomMatrix( ptr_matrix_b, row_b , column_b );
     initMatrix( ptr_matrix_c , row_c , column_c , 0 );
     transposeMatrix( ptr_matrix_b , ptr_matrix_b_t , row_b , column_b );
 
     printf("Matrix_a\n");    
-    printMatrix( ptr_matrix_a , row_a , column_a );
+//    printMatrix( ptr_matrix_a , row_a , column_a );
     printf("Matrix_B\n");    
-    printMatrix( ptr_matrix_b , row_b , column_b );
+//    printMatrix( ptr_matrix_b , row_b , column_b );
 
+    // CPU process
     mulMatrixTranspose( ptr_matrix_a , ptr_matrix_b_t , ptr_matrix_c ,
             row_a , column_a , column_b );
 
+    // Create Event for manage about process
+    cudaEvent_t event_copy_matrix_b_t[ column_b / 32 ];
+    cudaEvent_t event_copy_matrix_a[ row_a / 32 ];
+    cudaEvent_t evnet_copy_c[ row_a / 32 ];
+    for( unsigned int run = 0 ; run < row_a / 32 ; run++ )
+    { 
+        cudaEventCreate( &event_copy_matrix_a[run] );
+        cudaEventCreate( &event_copy_c[ run ] );
+    }
+    for( unsigned int run = 0 ; run < column_b / 32 ; run++ )
+    {
+        cudaEventCreate( &event_copy_matrix_b_t[ run ] );
+    }
+
+    unsigned int size_copy_b_t = column_b * 32;
+    unsigned int size_copy_a = column_a * 32;
+    // Below variable about recoed time to run all process
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord( start  , stream_copy_c ); 
+    for( unsigned int run_a = 0 ; run_a < grid_dimension.x ; run_a++ )
+    {
+        // copy matrix a some part
+        cudaMemcpyAsync( &d_matrix_a[ run_a * size_copy_a ], 
+                &ptr_matrix_a[ run_a * size_copy_a ],
+                size_copy_a,
+                H2D,
+                stream_copy_a );
+        // record event copy matrix a
+        cudaEventRecord( event_copy_matrix_a[ run_a ] , stream_copy_a );
+        for( unsigned int run_b = 0 ; run_b < grid_dimension.y ; run_b++ )
+        {
+            if( run_a == 0 ) // Mean you never copy value c
+            {
+                // copy matrix_b some part for first time
+                cudaMemcpyAsync( &d_matrix_b_t[ run_b * size_copy_b_t ],
+                        &ptr_matrix_b_t[ run_b * size_copy_b_t ],
+                        H2D,
+                        stream_copy_b );
+                // record event copy matrix b
+                cudaEventRecord( event_copy_matrix_b_t[ run_b ] , stream_copy_b );
+                // stream_calculate must to wait copy matrix b for the first time
+                cudaStreamWaitEvent( stream_calculate , event_copy_matrix_b_t[ rub_b ] );        
+            }
+            if( run_b = 0 ) // beforce calculate you must wait copy a finish
+            {
+                // stream_calculate first process muset wait copy a finish
+                cudaStreamWaitEvent( stream_calculate , event_copy_matrix_a[ run_a ] );
+            }
+            // kernal add to stream_calculate for calculate data
+            kernalMulMatrixTranspose<<< 1 , block_dimension , 0 , stream_calculate >>>(
+                    d_matrix_a , d_matrix_b_t , d_matrix_c , column_a ,
+                    run_a * grid_dimension.x , run_b * grid_dimension.y );
+        }
+        // Add event you can copy matrix c
+        cudaEventRecord( event_copy_matrix_c[ run_a ] , stream_calculate );
+        // stream copy c wait for can copy
+        cudaStreamWaitEvent( stream_copy_c , event_copy_matrix_c[ run_a ] );
+        // command add part copy matrix c to stream copy c
+        cudaMemcpyAsync( &ptr_matrix_c[ run_a * size_copy_a ],
+                &d_matrix_c[ run_a * size_copy_a ],
+                size_copy_a,
+                D2H,
+                stream_copy_c );
+    }
+
+    cudaEventRecord( stop , stream_copy_c );
+    cudaDeviceSynchronize( stop );
+    float cpu_elapsed_time_ms;
+    cudaEventElapsedTime(&cpu_elapsed_time_ms, start, stop);
+    printf("Time elapsed on matrix multiplication of %dx%d . %dx%d on CPU: %f ms.\n\n", 
+            row_a, column_a, 
+            row_b, column_b,
+            cpu_elapsed_time_ms );
+
     printf("Matrix_C\n");    
-    printMatrix( ptr_matrix_c , row_a , column_b );
+//    printMatrix( ptr_matrix_c , row_a , column_b );
 
     free( ptr_matrix_a );
     free( ptr_matrix_b );
